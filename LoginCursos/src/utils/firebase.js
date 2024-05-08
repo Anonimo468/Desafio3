3d73bb60ed8d045fcbad67bd929698a480ff3963
Cabrera437 <124718225+Cabrera437@users.noreply.github.com>
2024-05-08 01:13:11 -0600

    Add files via upload

---
+++ LoginCursos/src/utils/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCoZL-Qcdo8ohD-qqc-JfE92ytMf_K6klg",
+  authDomain: "cursos-99981.firebaseapp.com",
+  projectId: "cursos-99981",
+  storageBucket: "cursos-99981.appspot.com",
+  messagingSenderId: "236665650081",
+  appId: "1:236665650081:web:26e26e38ff3c668770a9e0"
+};
+
+// Initialize Firebase
